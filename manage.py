d8992b1942e3cf6d2ba3debe6b1e475e534cca12
Aryn Harmon <achcello@gmail.com>
2019-02-23 20:02:56 -0600

    redo boilerplate THANK TIM

--- manage.py
+++ manage.py
@@ -1,22 +1,39 @@
-#!/usr/bin/env python
-import os
-import sys
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from api import create_app
+from api.models import db
+
+# sets up the app
+app = create_app()
+
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+# adds the python manage.py db init, db migrate, db upgrade commands
+manager.add_command("db", MigrateCommand)
+
+
+@manager.command
+def runserver():
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
+
+@manager.command
+def runworker():
+    app.run(debug=False)
+
+
+@manager.command
+def recreate_db():
+    """
+    Recreates a database. This should only be used once
+    when there's a new database instance. This shouldn't be
+    used when you migrate your database.
+    """
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "miser.settings")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError:
-        # The above import may fail for some other reason. Ensure that the
-        # issue is really that Django is missing to avoid masking other
-        # exceptions on Python 2.
-        try:
-            import django
-        except ImportError:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
-        raise
-    execute_from_command_line(sys.argv)
+    manager.run()
